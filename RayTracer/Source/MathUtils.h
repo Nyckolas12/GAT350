648c95e328c8d81add6eedfaad3d3f2e71620c13
Nyckolas12 <nmendez@student.neumont.edu>
2024-10-22 20:07:43 -0600

    RayTracer

---
+++ RayTracer/Source/MathUtils.h
@@ -0,0 +1,38 @@
+#pragma once
+
+template<typename T>
+inline T Lerp(const T& a, const T& b, float t)
+{
+	return static_cast<T>(a + (b - a) * t);
+}
+template<typename T>
+inline T Clamp(const T& value, const T& min, const T& max)
+{
+	return (value < min) ? min : (value > max) ? max : value;
+}
+
+ inline void QuadraticPoint(int x1, int y1, int x2, int y2, int x3, int y3, float t, int& x, int& y)
+{
+	float one_minus_t1 = 1 - t;
+	float a1 = one_minus_t1 * one_minus_t1;
+	float b1 = 2 * one_minus_t1 * t;
+	float c1 = t * t;
+
+	x = (int)(a1 * x1 + b1 * x2 + c1 * x3);
+	y = (int)(a1 * y1 + b1 * y2 + c1 * y3);
+}
+
+inline void CubicPoint(int x1, int y1, int x2, int y2, int x3, int y3, int x4 , int y4, float t, int& x, int& y)
+{
+	float omt = 1 - t;
+
+	float a = omt * omt * omt;
+	float b = 3 * (omt * omt) * t;
+	float c = 3 * omt * (t * t);
+	float d = t * t * t;
+
+	x = (int)((a * x1) + (b * x2) + (c * x3) + (d * x4));
+	y = (int)((a * y1) + (b * y2) + (c * y3) + (d * y4));
+	//x = (int)(a1 * x1 + b1 * x2 + c1 * x3);
+	//y = (int)(a1 * y1 + b1 * y2 + c1 * y3);
+}
---
+++ RayTracer/Source/Model.cpp
@@ -0,0 +1,80 @@
+#include "Model.h"
+#include "Framebuffer.h"
+#include "Camera.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+/// <summary>
+/// Convert point from model space to world space
+/// </summary>
+/// <param name="framebuffer">The buffer we are drawing to</param>
+/// <param name="model">the model matrix that holds the points in model space</param>
+void Model::Draw(Framebuffer& framebuffer, const glm::mat4& model, const Camera& camera)
+{
+	for (int i = 0; i < m_verticies.size() -2; i += 3)
+	{
+		//
+		Vertex_t p1 = model * glm::vec4{ m_verticies[i], 1 };
+		Vertex_t p2 = model * glm::vec4{ m_verticies[i + 1], 1 };
+		Vertex_t p3 = model * glm::vec4{ m_verticies[i + 2], 1 };
+
+		p1 = camera.ModelToView(p1);
+		p2 = camera.ModelToView(p2);
+		p3 = camera.ModelToView(p3);
+
+		glm::ivec2 s1 = camera.ToScreen(p1);
+		glm::ivec2 s2 = camera.ToScreen(p2);
+		glm::ivec2 s3 = camera.ToScreen(p3);
+
+		framebuffer.DrawTrianlge(s1.x, s1.y, s2.x, s2.y, s3.x, s3.y, m_color);
+	}
+}
+
+bool Model::Load(const std::string& filename)
+{
+
+	std::ifstream stream(filename);
+	if (!stream.is_open()) {
+		std::cerr << "Error opening ifstream" << std::endl;
+		return false;
+	}
+	Verticies_t vertices;
+	
+	std::string line;
+	while (std::getline(stream, line)) {
+		//read in vertex positions
+		if (line.substr(0, 2) == "v ") {
+			std::istringstream sstream{ line.substr(2) };
+			glm::vec3 position;
+			sstream >> position.x;
+			sstream >> position.y;
+			sstream >> position.z;
+			vertices.push_back(position);
+		}
+		else if (line.substr(0, 2) == "f ") {
+			std::istringstream sstream{ line.substr(2) };
+			std::string str;
+			while (std::getline(sstream, str, ' ')) {
+				std::istringstream sstream(str);
+				std::string indexString;
+
+				size_t i = 0;
+				unsigned int index[3] = { 0,0,0 };
+				while (std::getline(sstream, indexString, '/')) {
+					if (!indexString.empty()) {
+						std::istringstream indexStream{ indexString };
+						//stream indexstream into index[i]
+						indexStream >> index[i];
+					}
+					i++;
+				}
+				if (index[0] != 0) {
+					glm::vec3 position = vertices[index[0] - 1];
+					m_verticies.push_back(position);
+				}
+			}
+		}
+	}
+	stream.close();
+	return true;
+}
